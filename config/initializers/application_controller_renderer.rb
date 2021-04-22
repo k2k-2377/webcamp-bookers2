c9e09bf133cfae92c26402c79423d4f1a4099135
k2k-2377 <lebronkt23@gmail.com>
2021-04-22 08:22:42 +0000

    first submit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '78ba9faa15b579876e4ce157c37dc243e54e3f764f6073763b56782136440ef57f32a40234b6f273057502795e56aa395313db866de5a08ca3033c8b2ec7e290'
