5ce2d550e686f8bbe6b8666365aa2814a01871b0
k2k-2377 <lebronkt23@gmail.com>
2021-04-26 08:30:37 +0000

    second submit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image
 
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
   validates :name, uniqueness: true, presence: true, length: { in: 2..20 }
   validates :introduction, length: {maximum: 50}
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get '/home/about', to: 'homes#about'
   devise_for :users
   resources :users, only: [:show, :edit, :update, :index]
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
